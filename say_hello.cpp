775e893d287d441dd5898631b07b0e3149e3f7f2
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2014-08-26 22:27:58 -0500

    Fixed bugs

--- say_hello.cpp
+++ say_hello.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+
+
+int main() {
+    return 0;
+}
+
 
 void say_hello(const char* name) {
     std::cout << "Hello " <<  name << "!\n";
 }
 
+using namespace boost::python;
 
-int main(){return 0;}
-
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
-namespace boost {
-namespace python {
-
-BOOST_PYTHON_MODULE(hello)
+BOOST_PYTHON_MODULE(say_hello)
 {
+
     def("say_hello", say_hello);
-}
-}
 }
